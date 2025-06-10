6b2c31fad0edca2255457e8cbea81b7c4c4765e3
Amechi Aduba <132087137+amechi-aduba@users.noreply.github.com>
2025-06-09 21:15:46 -0400

    Delete react/src directory

--- react/src/pages/main-site/graphs-charts/handleBar.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { SciChartReact } from "scichart-react";
-import { drawBar } from "./drawBar";
-
-export default function BarChart() {
-  return <SciChartReact initChart={drawBar} />;
-}
